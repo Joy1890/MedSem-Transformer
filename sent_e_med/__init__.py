dfcc4c7597865ec55bbcc0c0fd6972bc4d127cf3
Joy1890 <186871207+Joy1890@users.noreply.github.com>
2026-05-03 19:07:28 -0400

    Clean package initialization file

--- sent_e_med/__init__.py
+++ sent_e_med/__init__.py
@@ -1,11 +1,3 @@
-"""
-Sent-e-Med: Clinical Risk Prediction Using Language Models.
-
-Reference:
-    Acharya et al. (2023). Clinical Risk Prediction Using Language Models:
-    Benefits and Considerations. arXiv:2312.03742
-"""
-
 from .config import SenteMedConfig
 from .model import SenteMed
 from .pretrain import pretrain, load_pretrained
